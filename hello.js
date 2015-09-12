c9f50b571778b8bff1b9de1d9df01626e9c311ac
Gregory Ivy <gpoi@mail.ru>
2015-09-12 19:02:32 +0300

    adding tabris maps

--- hello.js
+++ hello.js
@@ -1,20 +1,21 @@
+tabris.ui.set("background","red");
+
 var page = tabris.create("Page", {
-  title: "Hello, World!",
+  title: "City Jungle",
   topLevel: true
 });
 
-var button = tabris.create("Button", {
-  text: "Native Widgets",
-  layoutData: {centerX: 0, top: 100}
-}).appendTo(page);
+var drawer = tabris.create("Drawer");
 
-var label = tabris.create("TextView", {
-  font: "24px",
-  layoutData: {centerX: 0, top: [button, 50]}
-}).appendTo(page);
+tabris.create("PageSelector", {
+  layoutData: {left: 0, top: 200, right: 0, bottom: 0}
+}).appendTo(drawer);
 
-button.on("select", function() {
-  label.set("text", "Totally Rock!");
-});
+tabris.create("ESMap", {
+    layoutData: {left: 0, right: 0, top: 0, height: 200}
+  }).on("ready", function() {
+    this.set("center", [-33.867, 151.206]);
+    this.set("zoom", 13);
+}).appendTo(page);
 
 page.open();
